4f34430c6eb2b9ec8ba11df4e1d735ce5341b6a6
nobuosato <nobuosato@gmail.com>
2018-08-19 09:56:18 -0400

    update

--- tools/config.py
+++ tools/config.py
@@ -1,11 +1,10 @@
 
 conf = {}
 
-
 def load_config(fname):
     with open(fname) as f:
         L = f.readlines()
     D = {}
     for l in L:
-        exec l.replace('<<', '').replace('>>', '') in D
+        exec l in D
     conf.update(D['conf'])
