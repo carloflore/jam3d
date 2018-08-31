805e29af4543c68ba569e397f35405e28595896e
Alexey Prokudin <prokudin@jlab.org>
2018-08-31 16:55:05 -0400

    example02

--- examples/example02/input.py
+++ examples/example02/input.py
@@ -14,23 +14,23 @@ conf['block size']=10
 # params
 
 conf['params']={}
-
+# Parameters in gaussian approximation, parton model:
 conf['params']['pdf']={}
-conf['params']['pdf']['uv widths1']  ={'value':    5.72856e-01,'min': 0,'max':1,'fixed':False}
+conf['params']['pdf']['uv widths1']  ={'value':    3.99391e-01,'min': 0,'max':1,'fixed':False}
 conf['params']['pdf']['uv widths2']  ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':True}
-conf['params']['pdf']['dv widths1']  ={'value':    5.72856e-01,'min': 0,'max':1,'fixed':'uv widths1'}
+conf['params']['pdf']['dv widths1']  ={'value':    3.99391e-01,'min': 0,'max':1,'fixed':'uv widths1'}
 conf['params']['pdf']['dv widths2']  ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':'uv widths2'}
-conf['params']['pdf']['sea widths1'] ={'value':    6.03981e-01,'min': 0,'max':1,'fixed':False}
+conf['params']['pdf']['sea widths1'] ={'value':    5.44764e-01,'min': 0,'max':1,'fixed':False}
 conf['params']['pdf']['sea widths2'] ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':True}
 
 conf['params']['ff']={}
-conf['params']['ff']['pi+fav widths1']   ={'value':    1.16790e-01,'min': 0,'max':1,'fixed':False}
+conf['params']['ff']['pi+fav widths1']   ={'value':    1.26522e-01,'min': 0,'max':1,'fixed':False}
 conf['params']['ff']['pi+fav widths2']   ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':True}
-conf['params']['ff']['pi+unfav widths1'] ={'value':    1.37349e-01,'min': 0,'max':1,'fixed':False}
+conf['params']['ff']['pi+unfav widths1'] ={'value':    1.62336e-01,'min': 0,'max':1,'fixed':False}
 conf['params']['ff']['pi+unfav widths2'] ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':True}
-conf['params']['ff']['k+fav widths1']    ={'value':    1.32925e-01,'min': 0,'max':1,'fixed':False}
+conf['params']['ff']['k+fav widths1']    ={'value':    1.39083e-01,'min': 0,'max':1,'fixed':False}
 conf['params']['ff']['k+fav widths2']    ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':True}
-conf['params']['ff']['k+unfav widths1']  ={'value':    1.86073e-01,'min': 0,'max':1,'fixed':False}
+conf['params']['ff']['k+unfav widths1']  ={'value':    2.24566e-01,'min': 0,'max':1,'fixed':False}
 conf['params']['ff']['k+unfav widths2']  ={'value':    0.00000e+00,'min':-1,'max':1,'fixed':True}
 
 ############################################################################
@@ -39,6 +39,7 @@ conf['params']['ff']['k+unfav widths2']  ={'value':    0.00000e+00,'min':-1,'max
 conf['datasets']={}
 conf['datasets']['sidis']={}
 
+# The data sets to use are here:
 conf['datasets']['sidis']['xlsx']={}
 conf['datasets']['sidis']['xlsx'][1000]='sidis/expdata/1000.xlsx'  # |  proton   | pi+   | M_Hermes | hermes 
 conf['datasets']['sidis']['xlsx'][1001]='sidis/expdata/1001.xlsx'  # |  proton   | pi-   | M_Hermes | hermes 
@@ -52,8 +53,10 @@ conf['datasets']['sidis']['xlsx'][1007]='sidis/expdata/1007.xlsx'  # |  deuteron
 conf['datasets']['sidis']['norm']={}
 for idx in conf['datasets']['sidis']['xlsx']: conf['datasets']['sidis']['norm'][idx]={'value':1,'fixed':True,'min':0,'max':1} 
 
+
 conf['datasets']['sidis']['filters']={}
-for idx in conf['datasets']['sidis']['xlsx']: conf['datasets']['sidis']['filters'][idx]="z<0.6 and Q2>1.69 and pT>0.2 and pT<0.9"
+# The cuts you want to impose on the data are here:
+for idx in conf['datasets']['sidis']['xlsx']: conf['datasets']['sidis']['filters'][idx]="z>0.2 and z<0.6 and Q2>1.69 and (pT/z)**2>0.25*Q2 and dy>2.5"
 
 
 
